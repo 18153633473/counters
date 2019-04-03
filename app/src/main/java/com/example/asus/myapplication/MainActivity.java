a6bd6886bffcc724fdf063980a2105222a2810a3
Your Name <you@example.com>
2019-04-03 20:18:39 +0800

    篮球计分器

--- app/src/main/java/com/example/asus/myapplication/MainActivity.java
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -17,9 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView out = findViewById(R.id.textView2);
+        TextView out2 = findViewById(R.id.newtextView2);
+
         Button btn1 = findViewById(R.id.button1);
         Button btn2 = findViewById(R.id.button2);
         Button btn3 = findViewById(R.id.button3);
+        Button newbtn1 = findViewById(R.id.newbutton1);
+        Button newbtn2 = findViewById(R.id.newbutton2);
+        Button newbtn3 = findViewById(R.id.newbutton3);
         Button btn4 = findViewById(R.id.buttons);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,11 +60,47 @@ public class MainActivity extends AppCompatActivity {
                 out.setText(result);
             }
         });
+        newbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView out2 = findViewById(R.id.newtextView2);
+                String str = out2.getText().toString();
+                int num = Integer.parseInt(str);
+                num = num + 1;
+                String result =String.valueOf(num);
+                out2.setText(result);
+            }
+        });
+        newbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView out2 = findViewById(R.id.newtextView2);
+                String str = out2.getText().toString();
+                int num = Integer.parseInt(str);
+                num = num + 2;
+                String result =String.valueOf(num);
+                out2.setText(result);
+            }
+        });
+        newbtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView out2 = findViewById(R.id.newtextView2);
+                String str = out2.getText().toString();
+                int num = Integer.parseInt(str);
+                num = num + 3;
+                String result =String.valueOf(num);
+                out2.setText(result);
+            }
+        });
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TextView out = findViewById(R.id.textView2);
+                TextView out2 = findViewById(R.id.newtextView2);
                 out.setText("0");
+                out2.setText("0");
             }
         });
 
