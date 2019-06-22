9e787c3dc25976acfa4b2dae70822781523a2054
Your Name <you@example.com>
2019-06-22 15:24:20 +0800

    这是平时作业

--- app/src/main/java/com/example/asus/myapplication/ViewpageActivity.java
+++ app/src/main/java/com/example/asus/myapplication/ViewpageActivity.java
@@ -5,7 +5,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ViewpageActivity extends AppCompatActivity {
+public class  ViewpageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
